4a3d456b8f23ae0e5d22c7d21be037174f1589cb
bmoses00 <34358568+bmoses00@users.noreply.github.com>
2019-03-13 00:01:55 -0400

    Add files via upload

---
+++ Element.java
@@ -0,0 +1,17 @@
+public class Element {
+    int intValue;
+    double doubleValue;
+    String stringValue;
+    int type;
+
+    public Element(int intValue
+                 , double doubleValue
+                 , String stringValue
+                 , int type) {
+        this.intValue = intValue;
+        this.doubleValue = doubleValue;
+        this.stringValue = stringValue;
+        this.type = type;
+    }
+
+}
